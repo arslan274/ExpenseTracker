55bdb9ed1c4b14c4c9fe1893e9ca4114b82a9a9c
arslan274 <arslanshaikh7106@icloud.com>
2025-10-20 22:10:23 +0100

    Created ExpenseManager.h and ExpenseManager.cpp

---
+++ include/ExpenseManager.h
@@ -0,0 +1,13 @@
+//
+// Created by Arslan Shaikh on 20/10/2025.
+//
+
+#ifndef EXPENSETRACKER_EXPENSEMANAGER_H
+#define EXPENSETRACKER_EXPENSEMANAGER_H
+
+
+class ExpenseManager {
+};
+
+
+#endif //EXPENSETRACKER_EXPENSEMANAGER_H
