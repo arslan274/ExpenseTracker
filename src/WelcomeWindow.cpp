aa466f5db46c1d65ef939b86fb53b14024f378bd
arslan274 <arslanshaikh7106@icloud.com>
2025-11-12 12:58:29 +0000

    Update WelcomeWindow.cpp

--- src/WelcomeWindow.cpp
+++ src/WelcomeWindow.cpp
@@ -1,3 +1,29 @@
 //
 // Created by Arslan Shaikh on 12/11/2025.
 //
+
+#include "WelcomeWindow.h"
+
+WelcomeWindow::WelcomeWindow(QWidget *parent)
+    : QWidget(parent) {
+
+    setWindowTitle("Welcome");
+    resize(400, 250);
+
+    auto *layout = new QVBoxLayout(this);
+
+    auto *label = new QLabel("👋 Hello, welcome to Expense Tracker!", this);
+    label->setAlignment(Qt::AlignCenter);
+    label->setStyleSheet("font-size: 18px; font-weight: bold;");
+
+    auto *continueButton = new QPushButton("Continue", this);
+    continueButton->setStyleSheet("font-size: 16px; padding: 8px;");
+
+    layout->addStretch();
+    layout->addWidget(label);
+    layout->addSpacing(10);
+    layout->addWidget(continueButton, 0, Qt::AlignCenter);
+    layout->addStretch();
+
+    connect(continueButton, &QPushButton::clicked, this, &WelcomeWindow::continueClicked);
+}
