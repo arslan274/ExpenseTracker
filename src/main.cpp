8552c5e863a6c886f6eebb438cbf843b5f921a54
arslan274 <arslanshaikh7106@icloud.com>
2025-11-12 12:58:52 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <QApplication>
 #include "MainWindow.h"
+#include "WelcomeWindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    MainWindow window;
-    window.show();
+
+    WelcomeWindow welcome;
+    MainWindow mainWin;
+
+    QObject::connect(&welcome, &WelcomeWindow::continueClicked, [&]() {
+        welcome.close();
+        mainWin.show();
+    });
+
+    welcome.show();
     return app.exec();
 }
